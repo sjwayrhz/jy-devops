3eeee87e838a0991584243e85232ae20fac757ed
sjwayrhz <sjwayrhz@gmail.com>
2021-09-22 14:36:24 +0800

    x

---
+++ Jenkinslib/vars/deployApp.groovy
@@ -0,0 +1,13 @@
+def call(String app) {
+    switch (app) {
+        case demo-spring-a:
+            return hwy-1;            
+        break
+        case demo-spring-a:
+            return hwy-1; 
+        break
+        default:
+            return none;
+        break
+    }
+}
