aa5480ab250047440581a5621df76deade0c3cb8
sjwayrhz <sjwayrhz@gmail.com>
2021-09-22 13:14:47 +0800

    x

--- Jenkinslib/src/org/devops/tools.groovy
+++ Jenkinslib/src/org/devops/tools.groovy
@@ -5,6 +5,7 @@ def PrintMes(value,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'green' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ,
+              'yellow' : "\033[1;33m >>>>>>>>>>>${value}<<<<<<<<<<< \033[1;33m" ,
               'purple' : "\033[0;35m${value}\033[0m"]
     ansiColor('xterm') {
         println(colors[color])
