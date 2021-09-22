7a5e91105eca3556e4cdf1f5356d3d3dabb6eb6e
sjwayrhz <sjwayrhz@gmail.com>
2021-09-22 14:21:58 +0800

    x

--- Jenkinslib/vars/sayHi.groovy
+++ Jenkinslib/vars/sayHi.groovy
@@ -3,3 +3,17 @@
 def call(String name) {
   echo "Hello, ${name}."
 }
+
+def callDeploy(String app) {
+    switch (app) {
+        case demo-spring-a:
+            return hwy-1;            
+        break
+        case demo-spring-a:
+            return hwy-1; 
+        break
+        default:
+            return none;
+        break
+    }
+}
