80f40a01a4f5c55eeba7c328a0eef650ba81fc2b
sjwayrhz <sjwayrhz@gmail.com>
2021-09-23 14:09:07 +0800

    try map

--- Jenkinslib/src/org/devops/SampleMap.groovy
+++ Jenkinslib/src/org/devops/SampleMap.groovy
@@ -2,7 +2,8 @@
 package org.devops
 
 class SampleMap {
-  def map = [:]
-  map.put('demo-spring-a':'hwy-1')
-  map.put('demo-spring-b':'hwy-2')
+  def map = [
+    'demo-spring-a':'hwy-1',
+    'demo-spring-b':'hwy-2'
+  ]
 }
