7686f8f075615e0b6c4571f63c5c62dd0600471f
sjwayrhz <sjwayrhz@gmail.com>
2021-09-24 10:01:54 +0800

    multiMethod

--- Jenkinslib/vars/deployApp.groovy
+++ Jenkinslib/vars/deployApp.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
-def call () {
   def map = [
     "demo-spring-a": "hwy-1",
     "demo-spring-b": "hwy-2"
   ]
-}
 
---
+++ Jenkinslib/vars/multiMethod.groovy
@@ -0,0 +1,6 @@
+def foo() {
+  echo "Hello foo from vars/multiMethod.groovy"
+}
+def bar() {
+  echo "Hello bar from vars/multiMethod.groovy"
+}
