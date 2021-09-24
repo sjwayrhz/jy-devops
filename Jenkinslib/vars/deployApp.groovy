e19d23967890c352891fbbe953d9d389322583a7
sjwayrhz <sjwayrhz@gmail.com>
2021-09-24 09:37:19 +0800

    test map

--- Jenkinslib/vars/deployApp.groovy
+++ Jenkinslib/vars/deployApp.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
 
-def deployApp = [
-  "demo-spring-a": "hwy-1",
-  "demo-spring-b": "hwy-2"
-]
+def call () {
+  def map = [
+    "demo-spring-a": "hwy-1",
+    "demo-spring-b": "hwy-2"
+  ]
+}
+
