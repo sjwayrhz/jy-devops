c9014fedfc754b797e5c19c781063c833c95a4b1
sjwayrhz <i@sjhz.cf>
2021-12-18 19:04:10 +0800

    gitlab.rb fixed

--- Gitlab/sjhz.tk/gitlab.rb
+++ Gitlab/sjhz.tk/gitlab.rb
@@ -45,7 +45,7 @@
 ##! On AWS EC2 instances, we also attempt to fetch the public hostname/IP
 ##! address from AWS. For more details, see:
 ##! https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
-external_url 'https://gitlab.juneyaokc.com'  
+external_url 'https://gitlab.sjhz.tk'  
 
 ## Roles for multi-instance GitLab
 ##! The default is to have no roles enabled, which results in GitLab running as an all-in-one instance.
