1a9a4bcf10684b2ed72dba6e5f99cd561b9da47c
sjwayrhz <i@sjhz.cf>
2022-03-19 06:16:46 +0800

    ingress update

--- ingress-practices/ingress-nginx-v1.0.5/labels-on-master.sh
+++ ingress-practices/ingress-nginx-v1.0.5/labels-on-master.sh
@@ -3,4 +3,4 @@
 # sh labels-on-master.sh k8s-master-01
 # sh labels-on-master.sh k8s-master-02
 # sh labels-on-master.sh k8s-master-03
-kubectl label nodes $1 kubernetes.io/ingress: nginx
+kubectl label nodes $1 kubernetes.io/ingress=nginx-ingress
